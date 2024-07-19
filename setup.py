992adb97f267976c472497cf9048ca1a44e22627
nwinston <nwinston96@gmail.com>
2024-07-19 15:05:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 
 setup(
-    name='pre_commit_placeholder_package',
+    name='pre_commit_placeholder_package_test',
     version='0.0.0',
     install_requires=['mypy==1.11.0'],
 )
